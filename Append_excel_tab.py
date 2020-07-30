2911e2be41392344ab2129ae7e3e1287bb739aa9
holzhang <45290176+holzhang@users.noreply.github.com>
2020-07-30 14:17:37 +0800

    Add files via upload
    sometimes, we need to move the excel file to an existing one and keep the original format.

---
+++ Append_excel_tab.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from openpyxl import load_workbook
+import os
+
+## append excel tab without change the previous one
+# script is in another tab
+# test excel is aaa.xlsx, under the Archive File folder
+
+path = os.path.abspath(os.path.join(os.getcwd(),'..'))
+path=path + '/Archive File/aaa.xlsx'
+
+book = load_workbook(path)
+writer = pd.ExcelWriter(path, engine = 'openpyxl')
+writer.book = book
+
+x3 = np.random.randn(100, 2)
+df3 = pd.DataFrame(x3)
+
+x4 = np.random.randn(100, 2)
+df4 = pd.DataFrame(x4)
+
+df3.to_excel(writer, sheet_name ='x3.xlsx')
+df4.to_excel(writer, sheet_name ='x4.xlsx')
+writer.save()
+writer.close()
